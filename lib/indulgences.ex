f2d75d37c51f973fe3419d78daf417285f4eb02b
TrsNium <trsnium@gmail.com>
2019-08-31 15:12:27 +0900

    add gitignore

---
+++ lib/indulgences.ex
@@ -0,0 +1,18 @@
+defmodule Indulgences do
+  @moduledoc """
+  Documentation for Indulgences.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Indulgences.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/indulgences_test.exs
@@ -0,0 +1,8 @@
+defmodule IndulgencesTest do
+  use ExUnit.Case
+  doctest Indulgences
+
+  test "greets the world" do
+    assert Indulgences.hello() == :world
+  end
+end
