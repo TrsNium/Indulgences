0f38b7543bc236905364c1730bb00e71c2ff09de
TrsNium <trsnium@gmail.com>
2019-09-02 23:45:22 +0900

    rename

--- lib/indulgences/activation.ex
+++ lib/indulgences/activation.ex
@@ -1,27 +1,27 @@
 defmodule Indulgences.Simulation.Activation do
   defstruct duration: nil, users: nil, method: nil
 
-  defmacro nothingFor(duration) do
+  defmacro nothing(duration) do
     [%__MODULE__{duration: duration, users: 0, method: :nothing}]
   end
 
-  defmacro nothingFor(activations, duration) do
+  defmacro nothing(activations, duration) do
     activations ++ [%__MODULE__{duration: duration, users: 0, method: :nothing}]
   end
 
-  defmacro atOnceUsers(users) do
-    [%__MODULE__{duration: 0, users: users, method: :atOnce}]
+  defmacro at_once(users) do
+    [%__MODULE__{duration: 0, users: users, method: :at_once}]
   end
 
-  defmacro atOnceUsers(activations, users) do
-    activations ++ [%__MODULE__{duration: 0, users: users, method: :atOnce}]
+  defmacro at_once(activations, users) do
+    activations ++ [%__MODULE__{duration: 0, users: users, method: :at_once}]
   end
 
-  defmacro constantUsersPerSec(duration, users) do
+  defmacro constant_users_per_sec(duration, users) do
     [%__MODULE__{duration: duration, users: users, method: :constant}]
   end
 
-  defmacro constantUsersPerSec(activations, duration, users) do
+  defmacro constant_users_per_sec(activations, duration, users) do
     activations ++ [%__MODULE__{duration: duration, users: users, method: :constant}]
   end
 end
