742b35f40b1ca8c8923068be7d9dc388eebc8d2d
TrsNium <trsnium@gmail.com>
2019-09-10 23:52:12 +0900

    fix time unit

--- lib/indulgences/activation/constant/engine.ex
+++ lib/indulgences/activation/constant/engine.ex
@@ -6,9 +6,10 @@ defmodule Indulgences.Activation.Constant.Engine do
   alias Indulgences.Scenario
 
   def execute(%Activation{}=activation, %Scenario{}=scenario, master) do
-    end_time = Time.add(Time.utc_now, activation.duration, :second)
+    microsecond_duration = activation.duration * 1_000_000
+    end_time = Time.add(Time.utc_now, microsecond_duration, :microsecond)
     total_users= activation.users*activation.duration
-    exec(0, total_users, activation.duration, end_time, scenario, master)
+    exec(0, total_users, microsecond_duration, end_time, scenario, master)
   end
 
   defp exec(done_users, total_users, _, _, _, _) when done_users == total_users do
@@ -16,7 +17,7 @@ defmodule Indulgences.Activation.Constant.Engine do
   end
 
   defp exec(done_users, total_users, duration, end_time, %Scenario{}=scenario, master) do
-    progress_rate = 1 - (Time.diff(end_time, Time.utc_now, :second) / duration)
+    progress_rate = 1 - (Time.diff(end_time, Time.utc_now, :microsecond) / duration)
     desired_done_users = Kernel.trunc(progress_rate * total_users)
     # TODO: Execute scenario with background
     updated_done_users= case desired_done_users > done_users do
